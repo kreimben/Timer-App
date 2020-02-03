c855bfd2d4f656f2536d52f70ccfe11a695c2962
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-02-03 21:25:21 +0900

    initial commit

---
+++ T!mer/Models/IAPManager.swift
@@ -0,0 +1,15 @@
+import UIKit
+import SwiftUI
+import StoreKit
+
+class IAPManager: NSObject {
+
+}
+
+//
+//  IAPManager.swift
+//  T!mer
+//
+//  Created by Aksidion Kreimben on 2/3/20.
+//  Copyright © 2020 Aksidion Kreimben. All rights reserved.
+//
