e15a4fc8b4df76bbc134ec15f312a025822a0866
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-02-10 17:40:28 +0900

    Reintegrate Pods

--- T!mer/View/SettingView/SettingPageView.swift
+++ T!mer/View/SettingView/SettingPageView.swift
@@ -18,6 +18,14 @@ struct SoundsArray: Identifiable, Hashable {
 
 struct SettingPageView: View {
     
+    init() {
+        
+        if self.userSettings.isUserPurchased {
+            
+            
+        }
+    }
+    
     @State var showingModal = false
     
     @ObservedObject var userSettings = UserSettings()
@@ -39,28 +47,28 @@ struct SettingPageView: View {
                     print("Buy In-app Purchase")
                     //                    self.userSettings.isUserPurchased.toggle()
                     let productID = "com.KreimbenPro.nonConsumable.removeads"
-                    SwiftyStoreKit.purchaseProduct(productID, quantity: 1, atomically: true) { result in
-                        
-                        switch result {
-                            
-                        case .success(let purchase):
-                            print("Purchase Success: \(purchase)")
-                            
-                        case .error(let error):
-                            switch error.code {
-                            case .unknown: print("Unknown error. Please contact support")
-                            case .clientInvalid: print("Not allowed to make the payment")
-                            case .paymentCancelled: break
-                            case .paymentInvalid: print("The purchase identifier was invalid")
-                            case .paymentNotAllowed: print("The device is not allowed to make the payment")
-                            case .storeProductNotAvailable: print("The product is not available in the current storefront")
-                            case .cloudServicePermissionDenied: print("Access to cloud service information is not allowed")
-                            case .cloudServiceNetworkConnectionFailed: print("Could not connect to the network")
-                            case .cloudServiceRevoked: print("User has revoked permission to use this cloud service")
-                            default: print((error as NSError).localizedDescription)
-                            }
-                        }
-                    }
+//                    SwiftyStoreKit.purchaseProduct(productID, quantity: 1, atomically: true) { result in
+//
+//                        switch result {
+//
+//                        case .success(let purchase):
+//                            print("Purchase Success: \(purchase)")
+//
+//                        case .error(let error):
+//                            switch error.code {
+//                            case .unknown: print("Unknown error. Please contact support")
+//                            case .clientInvalid: print("Not allowed to make the payment")
+//                            case .paymentCancelled: break
+//                            case .paymentInvalid: print("The purchase identifier was invalid")
+//                            case .paymentNotAllowed: print("The device is not allowed to make the payment")
+//                            case .storeProductNotAvailable: print("The product is not available in the current storefront")
+//                            case .cloudServicePermissionDenied: print("Access to cloud service information is not allowed")
+//                            case .cloudServiceNetworkConnectionFailed: print("Could not connect to the network")
+//                            case .cloudServiceRevoked: print("User has revoked permission to use this cloud service")
+//                            default: print((error as NSError).localizedDescription)
+//                            }
+//                        }
+//                    }
                 }
                 
                 Button(action: {
