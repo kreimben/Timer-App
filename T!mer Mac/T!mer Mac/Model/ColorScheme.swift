637de0a8ec50e53c0d97bd77cccb04598692d05f
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-03-21 17:36:01 +0900

    Initial commit

---
+++ T!mer Mac/T!mer Mac/Model/ColorScheme.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+enum Colors {
+    case blue, red, yellow, green, purple, black, white, orange
+}
+
+class ColorScheme {
+    
+    func colorGenerator(color: Colors) {
+        
+        
+    }
+}
+
+//
+//  ColorScheme.swift
+//  T!mer Mac
+//
+//  Created by Aksidion Kreimben on 3/21/20.
+//  Copyright © 2020 Aksidion Kreimben. All rights reserved.
+//
