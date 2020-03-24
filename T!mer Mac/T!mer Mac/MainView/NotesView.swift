c002a6bf2b5cd067edbf75d1b8977b55609ce145
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-03-24 22:16:39 +0900

    Add tips at "NotesView"

--- T!mer Mac/T!mer Mac/MainView/NotesView.swift
+++ T!mer Mac/T!mer Mac/MainView/NotesView.swift
@@ -6,6 +6,10 @@ struct NotesView: View {
     @Binding var isPresented: Bool
     /// @END
     
+    /// @Background Color Opacity
+    @State var backgroundColorOpacity = 0.7
+    /// @END
+    
     var body: some View {
         VStack {
             ScrollView {
@@ -17,35 +21,35 @@ struct NotesView: View {
                             .padding([.top])
                         Text("Powerful Visualized Timer")
                             .font(.system(size: 30, design: .rounded)).bold()
-                    }
+                    } // Title
                     
                     HStack {
                         Text("Focus on your task and Don't waste your time with")
                             .font(.system(size: 13, design: .rounded))
                         Text("\"T!mer\".")
                             .font(.system(size: 13, design: .rounded)).bold()
-                    }.padding([.top, .bottom])
+                    }.padding([.top, .bottom]) // Headline
                     
                     Image("main_screenshot")
                         .resizable()
                         .frame(width: 500 * 0.8, height: 300 * 0.8)
                     
-                    Text("How to Use T!mer?")
-                        .font(.system(.headline))
-                    
                     ZStack { // Shortcuts
-                        Color.blue.opacity(0.7)
+                        Color.blue.opacity(backgroundColorOpacity)
                         VStack {
                             Text("Shortcuts")
                                 .font(.system(.title, design: .rounded))
                                 .padding()
                             
-                            Text("T!mer have some of shortcuts for convinience to start T!mer.")
+                            Text("You can start T!mer drawing circle using trackpad or mouse")
                                 .font(.system(size: 12))
+                            
+                            Text("directly though T!mer have some of shortcuts for convinience.")
+                                .font(.system(size: 12)).padding(.bottom)
                             
                             Text("Let's look around!")
                                 .font(.system(size: 12))
-                                .padding([.leading, .trailing, .bottom])
+                                .padding([.leading, .trailing])
                             
                             Text("Run T!mer for").bold().padding()
                             VStack(alignment: .leading) {
@@ -129,7 +133,36 @@ struct NotesView: View {
                                 .padding(.bottom)
                         }
                     } // Shortcuts
-                        .padding([.bottom])
+                    
+                    ZStack { // Tips
+                        Color.yellow.opacity(backgroundColorOpacity)
+                        VStack {
+                            
+                            Text("Tips")
+                                .font(.system(size: 32, design: .rounded))
+                                .padding([.top, .bottom])
+                            
+                            VStack(alignment: .leading) {
+                                VStack(alignment: .leading) {
+                                    Text("⚬ How to Stop T!mer?")
+                                        .font(.system(.headline, design: .rounded))
+                                        .padding([.top, .bottom])
+                                    
+                                    Text("- Click and hold inner circle until timer is cancled or Press ⌘.")
+                                        .padding(.bottom)
+                                }
+                                
+                                VStack(alignment: .leading) {
+                                    Text("⚬ How to set T!mer 60 minutes?")
+                                        .font(.system(.headline, design: .rounded))
+                                        .padding([.top, .bottom])
+                                    
+                                    Text("- Click and hold circle or Press ⌘⌥6")
+                                        .padding(.bottom)
+                                }
+                            }
+                        }
+                    } // Tips
                 }
             } // ScrollView
             .frame(width: 400, height: 240)
