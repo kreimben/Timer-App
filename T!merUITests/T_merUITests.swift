1cf5d1a9df256662e037f33f3058894a7cd1006e
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-04-09 03:20:23 +0900

    Refine some codes

--- T!merUITests/T_merUITests.swift
+++ T!merUITests/T_merUITests.swift
@@ -7,23 +7,10 @@ class T_merUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         
-        // 1
         let app = XCUIApplication()
         setupSnapshot(app)
         app.launch()
-        // 2
-//        let chipCountTextField = app.textFields["chip count"]
-//        chipCountTextField.tap()
-//        chipCountTextField.typeText("10")
-        // 3
-//        let bigBlindTextField = app.textFields["big blind"]
-//        bigBlindTextField.tap()
-//        bigBlindTextField.typeText("100")
-        // 4
         snapshot("01UserEntries")
-        // 5
-//        app.buttons["what should i do"].tap()
-//        snapshot("02Suggestion")
     }
 
     func testLaunchPerformance() throws {
