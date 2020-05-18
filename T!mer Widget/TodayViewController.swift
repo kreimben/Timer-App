dbcd2586ca377143d4b6c076e81a8c5b32019e48
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-05-19 06:35:31 +0900

    Add "today view" (widget) feature - initial commit

---
+++ T!mer Widget/TodayViewController.swift
@@ -0,0 +1,31 @@
+import UIKit
+import SwiftUI
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+        
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
+
+//
+//  TodayViewController.swift
+//  T!mer Widget
+//
+//  Created by Aksidion Kreimben on 5/19/20.
+//  Copyright © 2020 Aksidion Kreimben. All rights reserved.
+//
