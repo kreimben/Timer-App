2a350fd7fe98c6a4935cf0286b528e5d9d277e55
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-05-28 18:17:28 +0900

    initial commit

---
+++ T!mer/View/Todo/TodoView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct TodoView: View {
+    var body: some View {
+        Text("Todo View Test")
+    }
+}
+
+struct TodoView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoView()
+    }
+}
+
+//
+//  TodoView.swift
+//  T!mer
+//
+//  Created by Aksidion Kreimben on 5/28/20.
+//  Copyright © 2020 Aksidion Kreimben. All rights reserved.
+//
