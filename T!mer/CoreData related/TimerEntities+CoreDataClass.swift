103f7df80b40883755b558eecd3a12f9b63d1331
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-05-29 21:26:37 +0900

    Fix CoreData

---
+++ T!mer/CoreData related/TimerEntities+CoreDataClass.swift
@@ -0,0 +1,15 @@
+import Foundation
+import CoreData
+
+
+public class TimerEntities: NSManagedObject {
+
+}
+
+//
+//  TimerEntities+CoreDataClass.swift
+//
+//
+//  Created by Aksidion Kreimben on 5/29/20.
+//
+//
---
+++ T!mer/CoreData related/TimerEntities+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+import Foundation
+import CoreData
+
+
+extension TimerEntities {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<TimerEntities> {
+        return NSFetchRequest<TimerEntities>(entityName: "TimerEntities")
+    }
+
+    @NSManaged public var memo: String?
+    @NSManaged public var notificationTime: Date?
+    @NSManaged public var text: String?
+
+}
+
+//
+//  TimerEntities+CoreDataProperties.swift
+//
+//
+//  Created by Aksidion Kreimben on 5/29/20.
+//
+//
