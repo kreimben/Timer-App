0d336290113958d150d4c7c868392e4f6f8997ee
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-07-16 01:56:59 +0900

    Add UnitTest scheme

---
+++ T!merTests(iOS)/T_merTests_iOS_.swift
@@ -0,0 +1,31 @@
+import XCTest
+
+@testable import T_mer
+
+class T_merTests_iOS_: XCTestCase {
+    
+    let mainController = MainController()
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testIsTimerRunning() {
+        
+        let result = self.mainController.isTimerRunning()
+        
+        XCTAssertEqual(result, true, "Failed!!!")
+    }
+}
+
+//
+//  T_merTests_iOS_.swift
+//  T!merTests(iOS)
+//
+//  Created by Aksidion Kreimben on 7/16/20.
+//  Copyright © 2020 Aksidion Kreimben. All rights reserved.
+//
