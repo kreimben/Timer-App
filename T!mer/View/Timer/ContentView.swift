0490a8f94f1d57769fe36a1962db41158cd665e3
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-10-05 14:15:30 +0900

    Refactoring

--- T!mer/View/Timer/ContentView.swift
+++ T!mer/View/Timer/ContentView.swift
@@ -10,18 +10,16 @@ struct ContentView: View {
     
     // MARK: - Init()
     init() {
-        
+
         let appearance = UINavigationBarAppearance()
-        
+
         appearance.configureWithOpaqueBackground()
         appearance.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0)
-        
+
         appearance.shadowColor = nil
         UINavigationBar.appearance().standardAppearance = appearance
         UINavigationBar.appearance().scrollEdgeAppearance = appearance
-        
-        UINavigationController().hidesBarsOnSwipe = true
-        
+
         self.selGen.prepare()
     }
     
