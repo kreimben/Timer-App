0e63c2520791ce35b4d1784b9cebb614b6db2955
Aksidion Kreimben <aksidionkreimben@gmail.com>
2020-10-12 18:00:10 +0900

    Initial commit

---
+++ T!mer/Resources/Notification.Name+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Notification.Name+Extensions.swift
+//  T!mer
+//
+//  Created by Aksidion Kreimben on 10/12/20.
+//  Copyright © 2020 Aksidion Kreimben. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static var changeTabBarColor = Notification.Name("changeTabBarColor")
+}
